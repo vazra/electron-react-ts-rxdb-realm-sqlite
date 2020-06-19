7e9835fcc6d12fffde17f8fc76a8a5d1184e895d
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-19 16:21:49 +0530

    build: :rocket: updated release script

--- ship.sh
+++ ship.sh
@@ -1,2 +1,14 @@
+# get the current branch 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# bump version
 yarn version --patch
+
+# checkout release branch
+git checkout release
+
+# rebase to current branch
+git rebase $BRANCH
+
+# push 
 git push && git push --tags
